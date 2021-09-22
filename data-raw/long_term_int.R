2d3f1170d80068efc5918072cc0b65cbf810b831
Dave Bosworth <David.Bosworth@water.ca.gov>
2021-09-21 17:14:31 -0700

    Developing raw data and data processing file and folder structure

---
+++ data-raw/long_term_int.R
@@ -0,0 +1,7 @@
+# Code to prepare long-term (1975-2021) integrated data sets:
+  # 1) Seasonal averages for each year for the entire Delta
+  # 2) Regional averages for each year
+
+
+
+#usethis::use_data(lt_integrated, overwrite = TRUE)
---
+++ data-raw/short_term_wq.R
@@ -0,0 +1,7 @@
+# Code to prepare `short_term_wq` (short-term water quality) data set:
+  # 2011-2021
+  # Monthly averages for each Region and Year
+
+
+
+#usethis::use_data(short_term_wq, overwrite = TRUE)
