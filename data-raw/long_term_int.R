86b6a7ff6533494376cdd7cbf20272e9ed05b8d4
Dave Bosworth <David.Bosworth@water.ca.gov>
2021-10-14 10:47:09 -0700

    Adding code to process long-term data for the WQ field measurement parameters

--- data-raw/long_term_int.R
+++ data-raw/long_term_int.R
@@ -2,15 +2,31 @@
   # 1) Seasonal averages for each year for the entire Delta
   # 2) Regional averages for each year
 
+
+# 1. Global Code ----------------------------------------------------------
+
+# Load packages
 library(tidyverse)
 library(lubridate)
+library(hms)
 library(readxl)
 library(magrittr)
+library(deltamapr)
+library(sf)
+
+# Import region assignments
+df_regions <- read_csv("data-raw/Rosies_regions.csv")
+
+## Load Delta regions shapefile from Brian
+sf_delta <- R_EDSM_Subregions_Mahardja_FLOAT %>%
+  # Filter to regions of interest
+  filter(SubRegion %in% unique(df_regions$SubRegion)) %>%
+  select(SubRegion)
 
 
-# 1. Water Quality --------------------------------------------------------
+# 2. Water Quality --------------------------------------------------------
 
-# 1.1 Hydrology -----------------------------------------------------------
+# 2.1 Hydrology -----------------------------------------------------------
 
 # Define file path for raw hydrology data
 fp_hydro <- "data-raw/Hydrology/"
@@ -120,17 +136,183 @@ rm(
   i
 )
 
-# 2. Primary Producers ----------------------------------------------------
+# 2.2 WQ Field measurements -----------------------------------------------
+
+# Load specific packages
+library(discretewq) # water quality data https://github.com/sbashevkin/discretewq
+library(rlang)
+library(dtplyr)
+
+# Import WQ field measurement data from the discretewq package
+# Select all long-term surveys (excluding EDSM and the USBR Sacramento ship channel study)
+df_wq <-
+  wq(
+    End_year = 2021,
+    Sources = c("EMP", "STN", "FMWT", "DJFMP", "SKT", "20mm", "Suisun", "Baystudy", "USGS")
+  )
+
+# Create function to summarize WQ field measurement data
+WQindices <- function(variable, type = "season", month.na = "strict") {
+
+  if(!month.na %in% c("strict", "relaxed")) {
+    stop("month.na must be either 'strict' or 'relaxed'")
+  }
+
+  if(!type %in% c("season", "year")) {
+    stop("type must be either 'season' or 'year'")
+  }
+
+  vardata <- df_wq %>%
+    # Remove any rows with NAs in our key variables
+    filter(!is.na(.data[[variable]]) & !is.na(Latitude) & !is.na(Datetime) & !is.na(Longitude) & !is.na(Date)) %>%
+    mutate(
+      # Convert to a timezone without daylight savings time
+      Datetime = with_tz(Datetime, tzone = "America/Phoenix"),
+      Date = with_tz(Date, tzone = "America/Phoenix"),
+      Station = paste(Source, Station),
+      # Create variable for time-of-day, not date
+      Time = as_hms(Datetime),
+      # Calculate difference from noon for each data point for later filtering
+      Noon_diff = abs(hms(hours = 12) - Time)
+    ) %>%
+    # Use dtplyr to speed up operations
+    lazy_dt() %>%
+    group_by(Station, Source, Date) %>%
+    # Select only 1 data point per station and date, choose data closest to noon
+    filter(Noon_diff == min(Noon_diff)) %>%
+    # When points are equidistant from noon, select earlier point
+    filter(Time == min(Time))%>%
+    ungroup() %>%
+    # End dtplyr operation
+    as_tibble() %>%
+    # Convert to sf object
+    st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326, remove = FALSE) %>%
+    # Change to crs of sf_delta
+    st_transform(crs = st_crs(sf_delta)) %>%
+    # Add subregions
+    st_join(sf_delta, join = st_intersects) %>%
+    # Remove any data outside our subregions of interest
+    filter(!is.na(SubRegion)) %>%
+    # Drop sf geometry column since it's no longer needed
+    st_drop_geometry() %>%
+    # Use dtplyr to speed up operations
+    lazy_dt() %>%
+    group_by(SubRegion) %>%
+    # Calculate variable SD for each subregion, then the number of SD's each data point exceeds the mean
+    mutate(
+      var_sd = sd(.data[[variable]]),
+      var_ext = (.data[[variable]] - mean(.data[[variable]]))/var_sd
+    ) %>%
+    ungroup() %>%
+    # End dtplyr operation
+    as_tibble() %>%
+    # Filter out any data points that are more than 10 SDs away from the mean of each subregion
+    filter(var_ext < 10) %>%
+    mutate(
+      # Move Decembers to the following year
+      Year = if_else(Month == 12, Year + 1, Year),
+      # Create seasonal variables
+      Season = case_when(
+        Month %in% 3:5 ~ "Spring",
+        Month %in% 6:8 ~ "Summer",
+        Month %in% 9:11 ~ "Fall",
+        Month %in% c(12, 1, 2) ~ "Winter",
+        TRUE ~ NA_character_
+      )
+    ) %>%
+    # Add region designations
+    left_join(df_regions, by = c("Season", "SubRegion")) %>%
+    # Remove any data not chosen for the long-term analysis
+    filter(Long_term) %>%
+    # Use dtplyr to speed up operations
+    lazy_dt() %>%
+    group_by(Month, Season, Region, Year) %>%
+    # Calculate monthly mean and sample size
+    summarize(
+      var_month_mean = mean(.data[[variable]]),
+      N = n()
+    ) %>%
+    ungroup() %>%
+    # End dtplyr operation
+    as_tibble() %>%
+    {if (month.na == "strict") {
+      # Fill in NAs for variable (and 0 for N) for any missing month, Region, year combinations to make sure all months are represented in each season
+      complete(., nesting(Month, Season), Region, Year, fill = list(N = 0))
+    } else {
+      # Fill in NAs for variable (and 0 for N) for any missing season, Region, year combinations to make sure all seasons and regions are represented when averaging
+      complete(., Season, Region, Year, fill = list(N = 0))
+    }} %>%
+    # Use dtplyr to speed up operations
+    lazy_dt() %>%
+    group_by(Season, Region, Year) %>%
+    # Calculate seasonal mean variable and total seasonal sample size
+    summarize(
+      var_mean = mean(var_month_mean),
+      N = sum(N)
+    ) %>%
+    ungroup() %>%
+    # End dtplyr operation
+    as_tibble()
+
+
+  variable2 <- sym(variable)
+
+  out <- vardata %>%
+    filter(Year %in% 1975:2021) %>%
+    {if (type == "season") {
+      # Group by season in order to calculate seasonal averages for entire Delta
+      group_by(., Season, Year)
+    } else {
+      # Group by region in order to calculate regional averages for each year
+      group_by(., Region, Year)
+    }} %>%
+    summarize(
+      {{variable}} := mean(var_mean),
+      "N_{{variable2}}" := sum(N),
+      .groups = "drop"
+    )
+
+  cat(paste("\nFinished", variable, "\n"))
+
+  return(out)
+}
+
+# Define WQ variables to summarize
+vars <- c("Temperature", "Secchi", "Salinity")
+
+# Seasonal averages for each year - relaxing the requirement of 3 months being present in all years
+lst_wq_season <- map(vars, WQindices, month.na = "relaxed", type = "season")
+
+#yearseasons <- expand_grid(Season = c("Winter", "Spring", "Summer", "Fall"), Year = 1975:2021)
+
+df_wq_season_f <- reduce(lst_wq_season, left_join) %>%
+  select(Year, Season, Temperature, Secchi, Salinity) %>%
+  arrange(Year, Season)
+
+# Yearly averages for each region - relaxing the requirement of 3 months being present in all years
+lst_wq_region <- map(vars, WQindices, month.na = "relaxed", type = "year")
+
+#yearregion <- expand_grid(Region = unique(df_regions$Region), Year = 1975:2021)
+
+df_wq_region_f <- reduce(lst_wq_region, left_join) %>%
+  select(Year, Region, Temperature, Secchi, Salinity) %>%
+  arrange(Year, Region)
+
+# Clean up
+rm(df_wq, lst_wq_season, lst_wq_region, vars, WQindices)
+
+
+# 3. Primary Producers ----------------------------------------------------
 
 
 
 
-# 3. Lower Trophic --------------------------------------------------------
+# 4. Lower Trophic --------------------------------------------------------
 
 
 
 
-# 4. Fish -----------------------------------------------------------------
+# 5. Fish -----------------------------------------------------------------
 
 
 
