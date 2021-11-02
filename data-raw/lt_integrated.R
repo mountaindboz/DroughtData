6544ca73540794eb6c4c0739345bf08344e26a65
Dave Bosworth <David.Bosworth@water.ca.gov>
2021-11-02 12:50:39 -0700

    Adding additional comments to the processing script for the long term integrated data set

--- data-raw/lt_integrated.R
+++ data-raw/lt_integrated.R
@@ -1,6 +1,6 @@
 # Code to prepare long-term (1975-2021) integrated data sets:
-  # 1) Seasonal averages for each year for the entire Delta
-  # 2) Regional averages for each year
+  # 1) `lt_seasonal` - Seasonal averages for each year for the entire Delta
+  # 2) `lt_regional` - Regional averages for each year
 
 
 # 1. Global Code ----------------------------------------------------------
