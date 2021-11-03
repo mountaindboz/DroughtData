6e10e74330be9423418821fceb99fec58f21923c
Dave Bosworth <David.Bosworth@water.ca.gov>
2021-11-03 10:22:52 -0700

    Decided to use direct online links to the CNRA data portal for the Dayflow data

--- data-raw/lt_integrated.R
+++ data-raw/lt_integrated.R
@@ -32,9 +32,9 @@ sf_delta <- R_EDSM_Subregions_Mahardja_FLOAT %>%
 fp_hydro <- "data-raw/Hydrology/"
 
 # Import DAYFLOW Data: https://data.cnra.ca.gov/dataset/dayflow
-df_dayflow_1970_1983 <- read_csv(file.path(fp_hydro, "dayflow-results-1970-1983.csv"))
-df_dayflow_1984_1996 <- read_csv(file.path(fp_hydro, "dayflow-results-1984-1996.csv"))
-df_dayflow_1997_2020 <- read_csv(file.path(fp_hydro, "dayflow-results-1997-2020.csv"))
+df_dayflow_1970_1983 <- read_csv("https://data.cnra.ca.gov/dataset/06ee2016-b138-47d7-9e85-f46fae674536/resource/a0a46a1d-bec5-4db9-b331-655e306860ba/download/dayflow-results-1970-1983.csv")
+df_dayflow_1984_1996 <- read_csv("https://data.cnra.ca.gov/dataset/06ee2016-b138-47d7-9e85-f46fae674536/resource/cb04e626-9729-4105-af81-f6e5a37f116a/download/dayflow-results-1984-1996.csv")
+df_dayflow_1997_2020 <- read_csv("https://data.cnra.ca.gov/dataset/06ee2016-b138-47d7-9e85-f46fae674536/resource/21c377fe-53b8-4bd6-9e1f-2025221be095/download/dayflow-results-1997-2020.csv")
 
 # Rename "EXPORTS" to "EXPORT" in the 1997-2020 data to match other data sets
 df_dayflow_1997_2020 %<>% rename(EXPORT = EXPORTS)
