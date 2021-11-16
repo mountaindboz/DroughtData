91c4ae2346e9f7f41fcdc4fd1089b42598fff8ee
Dave Bosworth <David.Bosworth@water.ca.gov>
2021-11-15 16:44:11 -0800

    Updating script for long-term metrics to import local copies of the DAYFLOW and DTO data

--- data-raw/lt_integrated.R
+++ data-raw/lt_integrated.R
@@ -28,13 +28,23 @@ sf_delta <- R_EDSM_Subregions_Mahardja_FLOAT %>%
 
 # 2.1 Hydrology -----------------------------------------------------------
 
+# If it hasn't been done already, download and save local copies of the DAYFLOW data from
+  # the CNRA data portal and Delta Outflow data (DTO) from CDEC since the DAYFLOW files may
+  # change and the DTO data is provisional
+
+# Set download to TRUE if need to download and save DAYFLOW and DTO data
+download <- FALSE
+
+# Download and save DAYFLOW and DTO data if necessary
+if (download) {source("data-raw/download_hydro.R")}
+
 # Define file path for raw hydrology data
 fp_hydro <- "data-raw/Hydrology/"
 
-# Import DAYFLOW Data: https://data.cnra.ca.gov/dataset/dayflow
-df_dayflow_1970_1983 <- read_csv("https://data.cnra.ca.gov/dataset/06ee2016-b138-47d7-9e85-f46fae674536/resource/a0a46a1d-bec5-4db9-b331-655e306860ba/download/dayflow-results-1970-1983.csv")
-df_dayflow_1984_1996 <- read_csv("https://data.cnra.ca.gov/dataset/06ee2016-b138-47d7-9e85-f46fae674536/resource/cb04e626-9729-4105-af81-f6e5a37f116a/download/dayflow-results-1984-1996.csv")
-df_dayflow_1997_2020 <- read_csv("https://data.cnra.ca.gov/dataset/06ee2016-b138-47d7-9e85-f46fae674536/resource/21c377fe-53b8-4bd6-9e1f-2025221be095/download/dayflow-results-1997-2020.csv")
+# Import DAYFLOW data for 1970-2020
+df_dayflow_1970_1983 <- read_csv(file.path(fp_hydro, "dayflow_1970-1983.csv"))
+df_dayflow_1984_1996 <- read_csv(file.path(fp_hydro, "dayflow_1984-1996.csv"))
+df_dayflow_1997_2020 <- read_csv(file.path(fp_hydro, "dayflow_1997-2020.csv"))
 
 # Rename "EXPORTS" to "EXPORT" in the 1997-2020 data to match other data sets
 df_dayflow_1997_2020 %<>% rename(EXPORT = EXPORTS)
@@ -60,13 +70,13 @@ df_dayflow_v2 <- df_dayflow_v1 %>%
   mutate(X2 = if_else(is.na(X2), X2Hutton, X2)) %>%
   select(-X2Hutton)
 
-# Add future outflow (DTO) and X2 until DAYFLOW is available: https://cdec.water.ca.gov/dynamicapp/staMeta?station_id=DTO
-dto_2021 <- read_csv(file.path(fp_hydro, "DTO_23.csv")) %>%
-  mutate(Date = ymd(`OBS DATE`)) %>%
-  select(Date, OUT = VALUE)
+# Import Delta Outflow (DTO) from CDEC for WY 2021 until DAYFLOW data is available
+df_dto_2021 <- read_csv(file.path(fp_hydro, "dto_2021.csv")) %>%
+  mutate(Date = date(DateTime)) %>%
+  select(Date, OUT = Value)
 
 # Add DTO data to the dayflow data
-df_dayflow_v3 <- bind_rows(df_dayflow_v2, dto_2021)
+df_dayflow_v3 <- bind_rows(df_dayflow_v2, df_dto_2021)
 
 ##################### Calculate future X2 based on DAYFLOW documentation:
 ###
@@ -130,8 +140,9 @@ rm(
   df_dayflow_v1,
   df_dayflow_v2,
   df_dayflow_v3,
-  dto_2021,
+  df_dto_2021,
   hutton_x2,
+  download,
   fp_hydro,
   i
 )
