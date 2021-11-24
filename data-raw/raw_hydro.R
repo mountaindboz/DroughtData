fbf35baebb27697d8c70b8f84ef940d35a9de4fd
Dave Bosworth <David.Bosworth@water.ca.gov>
2021-11-23 16:49:18 -0800

    Migrating code to download and save copies of raw hydrology data to raw_hydro script

--- data-raw/raw_hydro.R
+++ data-raw/raw_hydro.R
@@ -6,6 +6,7 @@
 # Load packages
 library(tidyverse)
 library(lubridate)
+library(cder)
 
 # 1. Import data ----------------------------------------------------------
 
@@ -17,7 +18,31 @@ library(lubridate)
 download <- FALSE
 
 # Download and save DAYFLOW and DTO data if necessary
-if (download) {source("data-raw/download_hydro.R")}
+if (download) {
+  # Define directory for raw hydrology data
+  dir_hydro <- "data-raw/Hydrology/"
+
+  # Download DAYFLOW Data from CNRA portal: https://data.cnra.ca.gov/dataset/dayflow
+  dayflow_1970_1983 <- read_csv("https://data.cnra.ca.gov/dataset/06ee2016-b138-47d7-9e85-f46fae674536/resource/a0a46a1d-bec5-4db9-b331-655e306860ba/download/dayflow-results-1970-1983.csv")
+  dayflow_1984_1996 <- read_csv("https://data.cnra.ca.gov/dataset/06ee2016-b138-47d7-9e85-f46fae674536/resource/cb04e626-9729-4105-af81-f6e5a37f116a/download/dayflow-results-1984-1996.csv")
+  dayflow_1997_2020 <- read_csv("https://data.cnra.ca.gov/dataset/06ee2016-b138-47d7-9e85-f46fae674536/resource/21c377fe-53b8-4bd6-9e1f-2025221be095/download/dayflow-results-1997-2020.csv")
+
+  # Download and save Delta Outflow (DTO) from CDEC for WY 2021 until DAYFLOW data is available
+  dto_2021 <-
+    cdec_query(
+      stations = "DTO",
+      sensors = 23,
+      durations = "D",
+      start.date = "2020-10-01",
+      end.date = "2021-10-31"
+    )
+
+  # Save data as .csv files in the "data-raw/Hydrology" folder
+  dayflow_1970_1983 %>% write_csv(file.path(dir_hydro, "dayflow_1970-1983.csv"))
+  dayflow_1984_1996 %>% write_csv(file.path(dir_hydro, "dayflow_1984-1996.csv"))
+  dayflow_1997_2020 %>% write_csv(file.path(dir_hydro, "dayflow_1997-2020.csv"))
+  dto_2021 %>% write_csv(file.path(dir_hydro, "dto_2021.csv"))
+}
 
 # Define file path for raw hydrology data
 fp_hydro <- "data-raw/Hydrology/"
