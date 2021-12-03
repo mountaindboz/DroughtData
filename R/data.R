91a5a7b19074cf271daa9a9c39e0444c98ed0ada
Dave Bosworth <David.Bosworth@water.ca.gov>
2021-12-03 09:53:01 -0800

    Adding documentation for the raw WQ measurement data for 1975-2021

--- R/data.R
+++ R/data.R
@@ -108,3 +108,54 @@
 #' * Dayflow: <https://data.cnra.ca.gov/dataset/dayflow>
 #' * DTO data from CDEC: <https://cdec.water.ca.gov/dynamicapp/staMeta?station_id=DTO>
 "raw_hydro_1975_2021"
+
+#' @title Raw values for water quality measurements for 1975-2021
+#' @description A data set containing raw values of the water quality parameters
+#'   (water temperature, salinity, secchi depth) for 1975-2021. Seasonal and
+#'   Regional averages of this data are in the `lt_seasonal` and `lt_regional`
+#'   data sets, respectively. This data set contains only one data point per
+#'   `Station` and `Date` and excludes any data points that are more than 10
+#'   standard deviations away from the mean of each `SubRegion`. Used in the
+#'   analyses for the Drought Synthesis.
+#'
+#' @format data frame with 34,523 rows and 14 columns
+#' \describe{
+#'   \item{Source}{Name of the source dataset. Either EMP (DWR's Environmental
+#'     Monitoring Program), FMWT (CDFW's Fall Midwater Trawl survey), or STN
+#'     (CDFW's Summer Townet survey)}
+#'   \item{Station}{Location where measurement was collected}
+#'   \item{Latitude}{Latitude of `Station` in Decimal Degrees (WGS 84 Datum)}
+#'   \item{Longitude}{Longitude of `Station` in Decimal Degrees (WGS 84 Datum)}
+#'   \item{Region}{Region designation of `Station` as [defined by the Drought
+#'     Synthesis
+#'     team](https://github.com/mountaindboz/DroughtData/blob/master/data-raw/Rosies_regions.csv).
+#'     }
+#'   \item{SubRegion}{Subregion designation of `Station` as defined by the
+#'     `R_EDSM_Subregions_Mahardja_FLOAT` shapefile from the
+#'     [`deltamapr`](https://github.com/InteragencyEcologicalProgram/deltamapr)
+#'     package.}
+#'   \item{YearAdj}{Adjusted calendar year - December-November, with December of
+#'     the previous calendar year included with the following year. For example,
+#'     December of 1974 is assigned year "1975" in this data set.}
+#'   \item{Season}{Winter (Dec-Feb), Spring (Mar-May), Summer (Jun-Aug), or Fall
+#'     (Sept-Nov)}
+#'   \item{Month}{Integer representing the month}
+#'   \item{Date}{Calendar date of the measurement}
+#'   \item{Datetime}{Date and time of the measurement. Not available for all
+#'     measurements.}
+#'   \item{Temperature}{Water temperature in degrees Celsius from the
+#'     `discretewq` data package. Data from EMP and FMWT collected in 2021 were
+#'     provided directly from data collectors and should be considered
+#'     provisional.}
+#'   \item{Salinity}{Salinity value on the Practical Salinity Scale from the
+#'     `discretewq` data package. Data from EMP and FMWT collected in 2021 were
+#'     provided directly from data collectors and should be considered
+#'     provisional.}
+#'   \item{Secchi}{Secchi depth in centimeters from the `discretewq` data
+#'     package. Data from EMP and FMWT collected in 2021 were provided directly
+#'     from data collectors and should be considered provisional.}
+#' }
+#'
+#' @source
+#' `discretewq` data package (version 2.1.0): <https://github.com/sbashevkin/discretewq/tree/1af04438c961c6d51f46cc777cbb6a142c1e3b30>
+"raw_wq_1975_2021"
