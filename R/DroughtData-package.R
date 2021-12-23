b9a2ce882005068eef37e8b533ea945836fce363
Dave Bosworth <David.Bosworth@water.ca.gov>
2021-12-23 14:58:44 -0800

    Adding a helper function to calculate seasonal and regional averages of raw drought data

--- R/DroughtData-package.R
+++ R/DroughtData-package.R
@@ -1,5 +1,7 @@
 #' @keywords internal
 #' @importFrom tibble tibble
+#' @importFrom rlang .data
+#' @importFrom magrittr %>%
 "_PACKAGE"
 
 # The following block is used by usethis to automatically manage
