987dd3fd86da86210d1de35231435a0c3fb35680
Dave Bosworth <David.Bosworth@water.ca.gov>
2022-09-08 15:59:05 -0700

    Updating the raw long-term chlorophyll data set with the latest version of `discretewq`

--- data-raw/raw_chla.R
+++ data-raw/raw_chla.R
@@ -1,18 +1,19 @@
-# Code to prepare data set of raw discrete chlorophyll-a data:
+# Code to prepare data set of long-term raw discrete chlorophyll-a data:
   # `raw_chla_1975_2021` - raw values of discrete chlorophyll-a concentrations for
-    # 1975-2021 from the EMP and USGS_SFBS surveys
+    # 1975-2021 from the EMP, USGS_SFBS, and NCRO surveys
 
 # Load packages
-library(tidyverse)
+library(dplyr)
+library(tidyr)
+library(readr)
+library(dtplyr)
 library(lubridate)
-library(readxl)
-# Make sure we are using `discretewq` version 2.3.2
+library(hms)
+# library(readxl)
+# Make sure we are using `discretewq` version 2.3.2.9000, commit dbb2638e800e637f3f2c70d47c9faad68f95db07
 # install.packages("devtools")
-# devtools::install_github("sbashevkin/discretewq", ref = "v2.3.2")
+# devtools::install_github("sbashevkin/discretewq", ref = "dbb2638e800e637f3f2c70d47c9faad68f95db07")
 library(discretewq)
-# Make sure we are using `deltamapr` version 1.0.0, commit d0a6f9c22aa074f906176e99a0ed70f97f26fffd
-# devtools::install_github("InteragencyEcologicalProgram/deltamapr", ref = "d0a6f9c22aa074f906176e99a0ed70f97f26fffd")
-library(deltamapr)
 library(sf)
 library(here)
 
@@ -21,45 +22,16 @@ i_am("data-raw/raw_chla.R")
 
 # 1. Import Data ----------------------------------------------------------
 
-# Import discrete chlorophyll-a data from the discretewq package (v2.3.2)
-# Select EMP and USGS_SFBS since these are the only surveys that have collected
-  # long-term (1975-current) discrete chlorophyll-a data
-df_package <- wq(Sources = c("EMP", "USGS_SFBS"), End_year = 2021)
-
-# Import additional EMP discrete chlorophyll-a data for 2021 - provided from
-  # personal data request
-df_emp_2021 <-
-  read_excel(
-    path = here("data-raw/Chlorophyll/EMP_water_quality_Dec2020-Dec2021.xlsx"),
-    range = "A2:AA288",
-    col_types = "text",
-    na = c("", "N/A")
-  )
-
-# Import additional EMP field data for 2021 - this includes EZ station
-  # coordinates - provided from personal data request
-df_emp_field_2021 <-
-  read_excel(
-    path = here("data-raw/Chlorophyll/EMP_water_quality_Dec2020-Dec2021.xlsx"),
-    range = "A290:R576",
-    col_types = "text",
-    na = c("", "N/A")
-  )
-
-# Import EMP station coordinates from EDI
-df_emp_coord <- read_csv("https://portal.edirepository.org/nis/dataviewer?packageid=edi.458.4&entityid=827aa171ecae79731cc50ae0e590e5af")
-
-# Modify sf_delta to include an additional subregion - Grant Line Canal and Old River
-sf_delta_chla <- R_EDSM_Subregions_Mahardja_FLOAT %>%
-  # Filter to regions of interest
-  filter(SubRegion %in% c(unique(DroughtData:::df_regions$SubRegion), "Grant Line Canal and Old River")) %>%
-  select(SubRegion)
+# Import discrete chlorophyll-a data from the discretewq package
+# Select all surveys available that have at least 20 years of data. This
+  # includes EMP, USGS_SFBS, and NCRO.
+df_chla <- wq(Sources = c("EMP", "USGS_SFBS", "NCRO"), End_year = 2021)
 
 
-# 2. Clean and Combine Data -----------------------------------------------
+# 2. Clean Data -----------------------------------------------
 
-# discretewq data
-df_package_c <- df_package %>%
+# Prepare discrete chlorophyll-a data from the discretewq package
+df_chla_c <- df_chla %>%
   transmute(
     Source,
     Station,
@@ -69,141 +41,23 @@ df_package_c <- df_package %>%
     Date = date(Date),
     # Convert Datetime to PST
     Datetime = with_tz(Datetime, tzone = "Etc/GMT+8"),
-    Chlorophyll,
-    # No Chla values appear to be <RL - make all records "=" in Chlorophyll_Sign
-    Chlorophyll_Sign = "="
+    # Fill in "=" for the NA values in Chlorophyll_Sign
+    Chlorophyll_Sign = if_else(is.na(Chlorophyll_Sign), "=", Chlorophyll_Sign),
+    Chlorophyll
   ) %>%
-  # Remove records without Chlorophyll-a data
-  drop_na(Chlorophyll)
-
-# Prepare EMP station coordinates to be joined to 2021 EMP data
-df_emp_coord_c <- df_emp_coord %>%
-  select(Station, Latitude, Longitude) %>%
-  drop_na()
-
-# Prepare DWR_EMP EZ station coordinates for 2021 to be joined to 2021 DWR_EMP data
-df_emp_coord_ez21 <- df_emp_field_2021 %>%
-  select(
-    SampleCode = `Sample Code`,
-    Latitude_field = contains("Latitude"),
-    Longitude_field = contains("Longitude")
-  ) %>%
-  drop_na() %>%
-  mutate(across(ends_with("_field"), as.numeric))
-
-# 2021 EMP Data
-df_emp_2021_c <- df_emp_2021 %>%
-  # Select and standardize variable names
-  select(
-    Station = `Station Name`,
-    StationNumber = `Station Number`,
-    SampleCode = `Sample Code`,
-    Datetime = `Sample Date`,
-    Chlorophyll = contains("Chlorophyll")
-  ) %>%
-  # Parse Datetime (as PST) and create Date variable
-  mutate(
-    Datetime = mdy_hm(Datetime, tz = "Etc/GMT+8"),
-    Date = date(Datetime)
-  ) %>%
-  # Remove overlapping data
-  filter(year(Date) > 2020) %>%
   # Remove records without Chlorophyll-a data
   drop_na(Chlorophyll) %>%
-  mutate(
-    # Create Source variable
-    Source = "EMP",
-    # Standardize Stations
-    Station = case_when(
-      str_detect(Station, "^SF Estuarine") ~ StationNumber,
-      str_detect(Station, "Hood\\s-\\sC3A$") ~ "C3A",
-      str_detect(Station, "^NZ068") ~ "NZ068",
-      TRUE ~ str_extract(Station, ".+(?=\\s-\\s)")
-    ),
-    # Add a new variable to identify Chlorophyll-a values below the reporting limit
-    Chlorophyll_Sign = if_else(str_detect(Chlorophyll, "^<"), "<", "="),
-    # For the Chlorophyll-a values below the reporting limit, make them equal to
-      # the reporting limit and convert to numeric
-    Chlorophyll = as.numeric(
-      if_else(
-        Chlorophyll_Sign == "<",
-        str_remove(Chlorophyll, "^<"),
-        Chlorophyll
-      )
-    )
-  ) %>%
-  # Add station coordinates
-  left_join(df_emp_coord_c, by = "Station") %>%
-  left_join(df_emp_coord_ez21, by = "SampleCode") %>%
-  mutate(
-    Latitude = if_else(is.na(Latitude), Latitude_field, Latitude),
-    Longitude = if_else(is.na(Longitude), Longitude_field, Longitude)
-  ) %>%
-  # Select variable order
-  select(
-    Source,
-    Station,
-    Latitude,
-    Longitude,
-    Date,
-    Datetime,
-    starts_with("Chlorophyll")
-  )
-
-# Combine all discrete chlorophyll-a data
-df_all <- bind_rows(df_package_c, df_emp_2021_c)
-
-
-# 3. Clean All Raw Data ---------------------------------------------------
-
-# Begin to clean all raw data
-df_all_c <- df_all %>%
   # Remove records without latitude-longitude coordinates
   drop_na(Latitude, Longitude) %>%
-  # Convert to sf object
+  # Assign SubRegions to the stations
   st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326, remove = FALSE) %>%
-  # Change to crs of sf_delta_chla
-  st_transform(crs = st_crs(sf_delta_chla)) %>%
-  # Add subregions
-  st_join(sf_delta_chla, join = st_intersects) %>%
+  st_transform(crs = st_crs(DroughtData:::sf_delta)) %>%
+  st_join(DroughtData:::sf_delta, join = st_intersects) %>%
   # Remove any data outside our subregions of interest
   filter(!is.na(SubRegion)) %>%
-  # Drop sf geometry column since it's no longer needed
-  st_drop_geometry()
-
-# Run a couple of QC checks:
-# Are there cases where multiple samples were collected at the same station in one day?
-df_all_c %>%
-  count(Source, Station, Date) %>%
-  filter(n > 1)
-# NO- no need to filter out duplicates
-
-# Look for and remove outliers from the data set:
-# Flag data points that have modified z-scores greater than 15 grouped by subregion
-df_all_c %>%
-  group_by(SubRegion) %>%
-  mutate(
-    Chla_median = median(Chlorophyll),
-    Chla_mad = mad(Chlorophyll),
-    Chla_mod_zscore = if_else(
-      Chla_mad == 0,
-      NA_real_,
-      abs(0.6745 * (Chlorophyll - Chla_median) / Chla_mad)
-    ),
-    Chla_mod_zscore_flag = case_when(
-      is.na(Chla_mod_zscore) ~ FALSE,
-      Chla_mod_zscore > 15 ~ TRUE,
-      TRUE ~ FALSE
-    )
-  ) %>%
-  ungroup()
-# We won't remove these flagged outliers because all the higher values appeared
-  # to be real based on best professional judgment
-
-# Finish cleaning all raw data
-raw_chla_1975_2021 <- df_all_c %>%
+  st_drop_geometry() %>%
   # Add variables for adjusted calendar year, month, and season
-  # Adjusted calendar year: December-November, with December of the previous
+    # Adjusted calendar year: December-November, with December of the previous
     # calendar year included with the following year
   mutate(
     Month = month(Date),
@@ -216,12 +70,115 @@ raw_chla_1975_2021 <- df_all_c %>%
     )
   ) %>%
   # Restrict data to 1975-2021
-  filter(YearAdj %in% 1975:2021) %>%
-  # Add region designations
-  left_join(DroughtData:::df_regions, by = c("Season", "SubRegion")) %>%
-  # Assign SouthCentral Region to the Grant Line Canal and Old River subregion
-  mutate(Region = if_else(SubRegion == "Grant Line Canal and Old River", "SouthCentral", Region)) %>%
-  # Select variables to keep
+  filter(YearAdj %in% 1975:2021)
+
+# Are there cases where multiple samples were collected at the same station in one day?
+df_chla_c %>%
+  count(Source, Station, Date) %>%
+  filter(n > 1)
+# YES- we need to filter data so that there is only one sample per station-day
+
+# Create function to filter data so that there is only one sample per
+  # station-day by choosing the data point closest to noon
+filt_daily_dups <- function(df, param) {
+  # Look for any instances when more than 1 data point was collected at a station-day
+  df_dups <- df %>%
+    count(Source, Station, Date) %>%
+    filter(n > 1) %>%
+    select(-n)
+
+  # Fix duplicates
+  df_dups_fixed <- df %>%
+    inner_join(df_dups, by = c("Source", "Station", "Date")) %>%
+    drop_na(Datetime) %>%
+    mutate(
+      # Create variable for time
+      Time = as_hms(Datetime),
+      # Calculate difference from noon for each data point for later filtering
+      Noon_diff = abs(hms(hours = 12) - Time)
+    ) %>%
+    # Use dtplyr to speed up operations
+    lazy_dt() %>%
+    group_by(Station, Date) %>%
+    # Select only 1 data point per station and date, choose data closest to noon
+    filter(Noon_diff == min(Noon_diff)) %>%
+    # When points are equidistant from noon, select earlier point
+    filter(Time == min(Time)) %>%
+    ungroup() %>%
+    # End dtplyr operation
+    as_tibble() %>%
+    select(-c(Time, Noon_diff))
+
+  # Add back fixed duplicates and format data frame
+  df %>%
+    anti_join(df_dups, by = c("Source", "Station", "Date")) %>%
+    bind_rows(df_dups_fixed)
+}
+
+# Filter daily duplicates
+df_chla_c2 <- filt_daily_dups(df_chla_c, "Chlorophyll")
+
+# Make sure there is only one sample per station-day for each parameter
+df_chla_c2 %>%
+  count(Source, Station, Date) %>%
+  filter(n > 1)
+# No more duplicates now
+
+
+# 3. Filter Data Temporally and Spatially ---------------------------------
+
+# Not all of the subregions were sampled consistently from 1975-2021. To make
+  # sure that we only include the subregions that were sampled adequately, we will
+  # require that a subregion needs to have data for at least 75% of the 47 years
+  # between 1975 to 2021 (35 years) for each season.
+df_subreg_seas <- df_chla_c2 %>%
+  distinct(SubRegion, YearAdj, Season) %>%
+  count(SubRegion, Season, name = "NumYears") %>%
+  group_by(SubRegion) %>%
+  filter(min(NumYears) >= 35) %>%
+  ungroup()
+
+df_chla_filt_seas <- df_chla_c2 %>% filter(SubRegion %in% unique(df_subreg_seas$SubRegion))
+
+
+# 4. Remove Outliers ------------------------------------------------------
+
+# Create function to flag data points with modified z-scores greater than a
+  # specified threshold
+flag_modzscore <- function(df, param, threshold) {
+  df %>%
+    mutate(
+      tmp_median = median(.data[[param]]),
+      tmp_mad = mad(.data[[param]]),
+      ModZscore = if_else(
+        tmp_mad == 0,
+        NA_real_,
+        abs(0.6745 * (.data[[param]] - tmp_median) / tmp_mad)
+      ),
+      ModZscore_flag = case_when(
+        is.na(ModZscore) ~ FALSE,
+        ModZscore > threshold ~ TRUE,
+        TRUE ~ FALSE
+      )
+    ) %>%
+    select(!starts_with("tmp_"))
+}
+
+# Flag data points that have modified z-scores greater than 15 grouped by subregion
+df_chla_flag_mzc <- df_chla_filt_seas %>%
+  group_by(SubRegion) %>%
+  flag_modzscore("Chlorophyll", threshold = 15) %>%
+  ungroup()
+# We won't remove these flagged outliers because all the higher values appeared
+  # to be real based on best professional judgment
+
+
+# 5. Save and Export Data -------------------------------------------------
+
+# Finish cleaning all raw data
+raw_chla_1975_2021 <- df_chla_filt_seas %>%
+  left_join(DroughtData:::df_regions %>% distinct(SubRegion, Region)) %>%
+  # Define variable order
   select(
     Source,
     Station,
@@ -234,14 +191,11 @@ raw_chla_1975_2021 <- df_all_c %>%
     Month,
     Date,
     Datetime,
-    Chlorophyll,
-    Chlorophyll_Sign
+    Chlorophyll_Sign,
+    Chlorophyll
   ) %>%
   # Arrange data
   arrange(Date, Source, Station)
-
-
-# 4. Save and Export Data -------------------------------------------------
 
 # Save final data set of raw discrete chlorophyll-a concentrations as csv file
   # for easier diffing
